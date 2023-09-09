d0e79568f6213cd1cbe872f3c0d809e7d3e2e02b
Erick <erickungu82@gmail.com>
2023-09-09 19:41:54 +0300

    Add user input to the game
    Added the ability to choose their move through the an input box. Also
    changed the draw condition from else to comparing the computer's and
    user's input.

--- myscript.js
+++ myscript.js
@@ -2,13 +2,13 @@
 // The choices variable stores all the possible choices in an array;
 // The randomChoices variable stores the randomized selection; 
 
-const choices = ['rock' , 'paper', 'scissors'];
+const choices = ['rock', 'paper', 'scissors'];
 const randomChoices = Math.floor(Math.random() * choices.length);
 
 
 // Function to get the computers rendomized choice between rock,paper or scissors://
 
-function getComputerchoice(){
+function getComputerchoice() {
     let computerChoice = choices[randomChoices];
 
     return computerChoice;
@@ -16,41 +16,36 @@ function getComputerchoice(){
 
 // Function that takes the user's choice and the computer's and returns the winner://
 
-function playRound(computerSelection, playerSelection){
+function playRound(computerSelection) {
     computerSelection = getComputerchoice();
 
-    if (computerSelection == 'rock' && playerSelection == 'scissors'){
+    if (computerSelection == 'rock' && playerSelection == 'scissors') {
         let winner = 'Computer';
         return `${winner} wins!! Rock beats Scissors.`;
-    }
-    else if (computerSelection == 'scissors' && playerSelection == 'paper'){
+    } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
         winner = 'Computer';
         return `${winner} wins!! Scissors beats Paper.`;
-    }
-    else if(computerSelection == 'paper' && playerSelection == 'rock'){
+    } else if (computerSelection == 'paper' && playerSelection == 'rock') {
         winner = 'Computer';
         return `${winner} wins!! Paper beats Rock.`;
-    }
-    else if (computerSelection == 'rock' && playerSelection == 'paper'){
+    } else if (computerSelection == 'rock' && playerSelection == 'paper') {
         winner = 'User';
-        return `${winner} wins!! Paper beats Rock.`;  
-    }
-    else if(computerSelection == 'scissors' && playerSelection == 'rock'){
+        return `${winner} wins!! Paper beats Rock.`;
+    } else if (computerSelection == 'scissors' && playerSelection == 'rock') {
         winner = 'User';
-        return `${winner} Rock beats Scissors.`;
-    }
-    else if(computerSelection == 'paper' && playerSelection == 'scissors'){
+        return `${winner} wins!! Rock beats Scissors.`;
+    } else if (computerSelection == 'paper' && playerSelection == 'scissors') {
         winner = 'User';
-        return `${winner} Scissors beats Paper.`;
-    }
-
-    else{
-        return 'Draw!!'
+        return `${winner} wins!! Scissors beats Paper.`;
+    } else if (computerSelection == playerSelection) {
+        return `Draw!! You both selected ${computerSelection}.`;
+    } else {
+        console.error('Invalid selection');
     }
 }
 
-// The user inserts their prefered move to play: Should be case insesitive;
-
+// The user inserts their preferred move to play: Should be case insensitive;
+const playerSelection = prompt("Enter your play:").toLowerCase();
 
 // console.log(playRound(getComputerchoice()));
 
@@ -58,18 +53,14 @@ function playRound(computerSelection, playerSelection){
 // A five round game that takes the oneRound function and returns the winner for 5 rounds.
 // It has a for loop that has an incremental value `round` that increases to 5 rounds and ends once if reaches 5 rounds.
 
+// function game() {
+//     for (let round = 0; round < 5; round++) {
+//         const playerSelection = 'paper';
 
+//         const result = playRound(playerSelection);
 
-function game(){
-    for (let round = 0; round < 5; round++){
-        const playerSelection = 'paper';
-
-        const result = playRound(playerSelection);
-
-        console.log(`Round ${round+1}: ${result}`)
-    }
-}
-
-console.log(game())
-
-
+//         console.log(`Round ${round+1}: ${result}`)
+//     }
+// }
+// playerSelection = 'Paper';
+console.log(playRound())
