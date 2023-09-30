08862c13816785e6f1f328ae4e9432043ec1ce5e
Erick <erickungu82@gmail.com>
2023-09-30 19:26:42 +0300

    Fix bug that incremented the score regardless of the logic set

--- myscript.js
+++ myscript.js
@@ -12,12 +12,13 @@ let playerScore = 0
 
 
 
+
 // Select and create the elements that are needed in the playRound() function;
 
 let playerChoice = document.querySelector('.user')
 let compChoice = document.querySelector('.comp')
-let userScore = document.querySelector ('.user-score')
-let compScore = document.querySelector ('.comp-score')
+let userScore = document.querySelector('.user-score')
+let compScore = document.querySelector('.comp-score')
 let btnChoice = document.querySelectorAll('[data-selection]')
 let winner = document.querySelector('.winner')
 let spanWinner = document.createElement('span')
@@ -38,77 +39,76 @@ function getComputerchoice() {
 btnChoice.forEach(button => button.addEventListener('click', () => {
     playerSelection = button.getAttribute('data-selection')
     computerSelection = getComputerchoice();
-    
+    result = playRound(playerSelection, computerSelection)
+
+
+
     spanCompChoice.textContent = ` ${computerSelection}`
     spanPlayerChoice.textContent = ` ${playerSelection}`
     playerChoice.appendChild(spanPlayerChoice)
     compChoice.appendChild(spanCompChoice)
 
-    overallPlayerScore = getPlayerScore()
-    spanPlayerScore.textContent = ` ${overallPlayerScore}`
+    console.log(result)
+
+    if (result.includes('you win')) {
+        playerScore++
+    } else if (result.includes('you lose')) {
+        computerScore++
+    }
+
+    spanPlayerScore.textContent = ` ${playerScore}`
     userScore.appendChild(spanPlayerScore)
 
-    overallCompScore = getComputerScore()
-    spanCompScore.textContent = ` ${overallCompScore}`
+    spanCompScore.textContent = ` ${computerScore}`
     compScore.appendChild(spanCompScore)
+}))
 
-    overallWinner = getWinner()
-    
-    console.log(`${overallCompScore}: ${overallPlayerScore}`)
-
-    
-}
-))
-
-function playRound(){
-    if (playerSelection === computerSelection){
-        return('Draw')
-    }
-    else if(playerSelection =='rock'){
+function playRound() {
+    if (playerSelection === computerSelection) {
+        return ('Draw')
+    } else if (playerSelection == 'rock') {
         return (computerSelection == 'scissors') ? 'you win' : 'you lose'
-    }
-    else if(playerSelection =='scissors'){
+    } else if (playerSelection == 'scissors') {
         return (computerSelection == 'paper') ? 'you win' : 'you lose'
-    }
-    else if(playerSelection =='paper'){
+    } else if (playerSelection == 'paper') {
         return (computerSelection == 'rock') ? 'you win' : 'you lose'
     }
 }
 
-function getPlayerScore(){
-    result = playRound()
+// function getPlayerScore() {
+//     result = playRound()
 
-    if(result.includes('win')){
-        playerScore++;
-    }
+//     if (result.includes('win')) {
+//         playerScore++;
+//     }
 
-    return playerScore
-}
+//     return playerScore
 
-function getComputerScore(){
-    result = playRound()
+// }
 
-    if(result.includes('lose')){
-        computerScore++;
-    }
+// function getComputerScore() {
+//     result = playRound()
 
-    return computerScore
-}
+//     if (result.includes('lose')) {
+//         return computerScore++;
+//     }
 
-function getWinner(){
-    let winningScore = 5;
-    playerScore = getPlayerScore();
-    computerScore = getComputerScore()
+//     return computerScore
+// }
 
-    if(playerScore == winningScore){
-        gameWinner = 'User'
-    }
-    else if(computerScore == winningScore){
-        gameWinner = 'Computer'
-    }
+// function getWinner() {
+//     let winningScore = 5;
+//     playerScore = getPlayerScore();
+//     computerScore = getComputerScore()
 
-    return gameWinner;
-}
+//     if (playerScore == winningScore) {
+//         gameWinner = 'User'
+//     } else if (computerScore == winningScore) {
+//         gameWinner = 'Computer'
+//     }
+
+//     return gameWinner;
+// }
 
 
 
@@ -123,147 +123,11 @@ spanPlayerChoice.style.fontFamily = 'indie Flower'
 spanCompChoice.style.fontSize = '30px'
 spanPlayerChoice.style.fontSize = '30px'
 
-spanPlayerScore.style.fontFamily=='Indie Flower'
-spanCompScore.style.fontFamily=='Indie Flower'
+spanPlayerScore.style.fontFamily == 'Indie Flower'
+spanCompScore.style.fontFamily == 'Indie Flower'
 
-spanPlayerScore.style.fontSize=='30px'
-spanCompScore.style.fontSize== '30px'
+spanPlayerScore.style.fontSize == '30px'
+spanCompScore.style.fontSize == '30px'
 
-spanPlayerScore.style.color=='red'
-spanCompScore.style.color=='red'
-
-
-
-// function getUserChoice(choice) {
-//     let userSelection = choice;
-//     return userSelection;
-//   }
-  
-//   // Add click event listeners to the buttons
-//   btnRock.addEventListener('click', function() {
-//     getUserChoice('rock');
-//   });
-  
-//   btnScissors.addEventListener('click', function() {
-//     getUserChoice('scissors');
-//   });
-  
-//   btnPaper.addEventListener('click', function() {
-//     getUserChoice('paper');
-//   });
-
-// if (getUserChoice() == 'scissors'){
-//     console.log(true)
-// }
-// else{
-//     console.log(false)
-// }
-
-
-
-// btnRock.addEventListener('click',function play(){
-//     let computerSelection = getComputerchoice();
-//     let userSelection = 'rock';
-//     spanCompChoice.textContent = ` ${computerSelection}`
-//     spanUserChoice.textContent = ` ${userSelection}`
-//     userChoice.appendChild(spanUserChoice)
-//     compChoice.appendChild(spanCompChoice)
-// })
-    
-    
-// btnScissors.addEventListener('click',function play(){
-//     let computerSelection = getComputerchoice();
-//     userSelection = 'scissors'
-//     spanCompChoice.textContent = ` ${computerSelection}`
-//     spanUserChoice.textContent = ` ${userSelection}`
-//     userChoice.appendChild(spanUserChoice)
-//     compChoice.appendChild(spanCompChoice)
-// })
-    
-// btnPaper.addEventListener('click',function play(){
-//     let computerSelection = getComputerchoice();
-//     userSelection = 'paper'
-//     spanCompChoice.textContent = ` ${computerSelection}`
-//     spanUserChoice.textContent = ` ${userSelection}`
-//     userChoice.appendChild(spanUserChoice)
-//     compChoice.appendChild(spanCompChoice)
-// })
-
-
-
-// Function that takes the user's choice and the computer's and returns the winner://
-
-// function playRound(computerSelection) {
-//     computerSelection = getComputerchoice();
-
-//     if (computerSelection == 'rock' && userSelection == 'scissors') {
-//         return `Oh no!! Computer wins!! Rock beats Scissors.`;
-    
-
-//     } else if (computerSelection == 'scissors' && userSelection == 'paper') {
-//         return `We go again!! Computer wins!! Scissors beats Paper.`;
-    
-
-//     } else if (computerSelection == 'paper' && userSelection == 'rock') {
-//         return `Try Again! Computer wins!! Paper beats Rock.`;
-
-//     } else if (computerSelection == 'rock' && userSelection == 'paper') {
-//         return `Lets Go! You win!! Paper beats Rock.`;
-    
-
-//     } else if (computerSelection == 'scissors' && userSelection == 'rock') {
-//         return `Okay, I see you! You win!! Rock beats Scissors.`;
-    
-
-//     } else if (computerSelection == 'paper' && userSelection == 'scissors') {
-//         return `Cool!!! You win!! Scissors beats Paper.`;
-    
-
-//     } else if (computerSelection == userSelection) {
-//         return `It's a tie!! ${computerSelection} was selected.`;
-
-//     } else {
-//         return 'Invalid selection';
-//     }
-
-// }
-
-
-
-
-
-
-
-// A five round game that takes the oneRound function and returns the winner for 5 rounds.
-// It has a for loop that has an incremental value `round` that increases to 5 rounds and ends once it reaches 5 rounds.
-
-// function game() {
-//     //Declared two variables that will store the player's scores
-//     let userScore = 0;
-//     let computerScore = 0;
-
-//     for (let round = 0; round < 5; round++) {
-//         userSelection = prompt("Enter your play:").toLowerCase();
-//         const computerSelection = getComputerchoice(); // Corrected function name
-//         const result = playRound(userSelection, computerSelection);
-
-//         if (result.includes('Computer wins')) {
-//             computerScore++;
-//         } else if (result.includes('You win')) {
-//             userScore++;
-//         }
-
-//         console.log(`Round ${round + 1}: ${result}: ComputerScore: ${computerScore}: UserScore: ${userScore}`);
-//     }
-
-//     if (computerScore > userScore) {
-//         console.log('Oh no!! We lost. Try again');
-//     } else if (userScore > computerScore) {
-//         console.log('Lets go! We beat the machines');
-//     } else {
-//         console.log(`Its a tie, We didn't get them this time.`);
-//     }
-//     console.log(overallResult);
-// }
-
-// console.log(game())
+spanPlayerScore.style.color == 'red'
+spanCompScore.style.color == 'red'
